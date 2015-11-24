58cc2b3183029d7fafef8cb7ce95cc048ac20f72
bingbing2015 <chenyipin@chenyipindeimac.local>
2015-11-24 14:23:31 +0800

    Initial Commit

---
+++ testImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testImage
+//
+//  Created by 陈奕频 on 15/11/24.
+//  Copyright © 2015年 陈奕频. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testImage
+//
+//  Created by 陈奕频 on 15/11/24.
+//  Copyright © 2015年 陈奕频. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
