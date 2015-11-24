93ff104480c9d591581eb615df33531a74e8ab3d
bingbing2015 <chenyipin@chenyipindeiMac.local>
2015-11-24 16:01:08 +0800

    边框永久设置为1px

--- testImage/ViewController.m
+++ testImage/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#define UIColorFromRGB(rgbValue) \
+[UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16)) / 255.0 \
+green:((float)((rgbValue & 0xFF00) >> 8)) / 255.0 \
+blue:((float)(rgbValue & 0xFF)) / 255.0 alpha:1.0]
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *textimage;
 
 @end
 
@@ -17,6 +23,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //边框永久设置为1px
+    //    CGFloat inset  = (1 / [UIScreen mainScreen].scale) * 1.0f;
+    //scale ＝ 1 代表1px，2代表2px，3代表3px
+    CALayer * layer = self.textimage.layer;
+    layer.borderColor = UIColorFromRGB(0x6d6d6d).CGColor;
+    layer.borderWidth = 1.0 / [UIScreen mainScreen].scale;
 }
 
 - (void)didReceiveMemoryWarning {
